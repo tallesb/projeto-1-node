72e3826fdf3e2097bae9b485a270be65edc8c3c6
Talles Nascimento <barbosatalles9@gmail.com>
2023-04-14 18:47:49 -0300

    remover async do método read

--- stream/upload-task.js
+++ stream/upload-task.js
@@ -1,34 +1,36 @@
-import {parse} from 'csv';
 import fs from 'node:fs'
+import {parse} from 'csv';
 import {Readable} from 'node:stream'
 
 const tasksPath = new URL('./tasks.csv', import.meta.url)
 
 class UploadTasksCSVStream extends Readable {
-  async _read() {
+  _read() {
     const tasksStream = fs.createReadStream(tasksPath)
 
     const tasksParser = tasksStream.pipe(parse())
 
     let isFirstRow = true;
 
-    for await (const chunck of tasksParser) {
-      if(isFirstRow) {
-        isFirstRow = false
-        continue
+    (async () => {
+      for await (const chunck of tasksParser) {
+        if(isFirstRow) {
+          isFirstRow = false
+          continue
+        }
+        
+        const [title, description] = chunck
+  
+        const task = {
+          title, description
+        }
+  
+        const bufferedTask = Buffer.from(JSON.stringify(task))
+  
+        this.emit('data', bufferedTask)
       }
-      
-      const [title, description] = chunck
-
-      const task = {
-        title, description
-      }
-
-      const bufferedTask = Buffer.from(JSON.stringify(task))
-
-      this.emit('data', bufferedTask)
-    }
-
+    })()
+    
     this.push(null)
   }
 }
